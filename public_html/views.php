b35817dc15290b4b407f06a5facf949c1f5a5297
chriskl <chriskl>
2002-07-11 06:01:14 +0000

    Works in a register_globals = off env

--- public_html/views.php
+++ public_html/views.php
@@ -3,7 +3,7 @@
 	/**
 	 * Manage views in a database
 	 *
-	 * $Id: views.php,v 1.3 2002/05/15 09:57:55 chriskl Exp $
+	 * $Id: views.php,v 1.4 2002/07/11 06:01:14 chriskl Exp $
 	 */
 
 	// Include application functions
@@ -17,9 +17,9 @@
 	 * Function to save after editing a view
 	 */
 	function doSaveEdit() {
-		global $localData, $view, $formDefinition;
+		global $localData;
 		
-		$status = $localData->setView($view, $formDefinition);
+		$status = $localData->setView($_POST['view'], $_POST['formDefinition']);
 		if ($status == 0)
 			doProperties('View updated.');
 		else
@@ -30,13 +30,13 @@
 	 * Function to allow editing of a view
 	 */
 	function doEdit($msg = '') {
-		global $data, $localData, $misc, $database, $view;
+		global $data, $localData, $misc;
 		global $PHP_SELF, $strName, $strDefinition;
 		
-		echo "<h2>", htmlspecialchars($database), ": Views: ", htmlspecialchars($view), ": Edit</h2>\n";
+		echo "<h2>", htmlspecialchars($_REQUEST['database']), ": Views: ", htmlspecialchars($_REQUEST['view']), ": Edit</h2>\n";
 		$misc->printMsg($msg);
 		
-		$viewdata = &$localData->getView($view);
+		$viewdata = &$localData->getView($_REQUEST['view']);
 		
 		if ($viewdata->recordCount() > 0) {
 			echo "<form action=\"$PHP_SELF\" method=post>\n";
@@ -48,29 +48,29 @@
 				htmlspecialchars($viewdata->f[$data->vwFields['vwdef']]), "</textarea></td></tr>\n";
 			echo "</table>\n";
 			echo "<input type=hidden name=action value=save_edit>\n";
-			echo "<input type=hidden name=view value=\"", htmlspecialchars($view), "\">\n";
-			echo "<input type=hidden name=database value=\"", htmlspecialchars($database), "\">\n";
+			echo "<input type=hidden name=view value=\"", htmlspecialchars($_REQUEST['view']), "\">\n";
+			echo "<input type=hidden name=database value=\"", htmlspecialchars($_REQUEST['database']), "\">\n";
 			echo "<input type=submit value=Save> <input type=reset>\n";
 			echo "</form>\n";
 		}
 		else echo "<p>No data.</p>\n";
 		
-		echo "<p><a class=navlink href=\"$PHP_SELF?database=", urlencode($database), "\">Show All Views</a> |\n";
-		echo "<a class=navlink href=\"$PHP_SELF?action=properties&database=", urlencode($database), "&view=", 
-			urlencode($view), "\">Properties</a></p>\n";
+		echo "<p><a class=navlink href=\"$PHP_SELF?database=", urlencode($_REQUEST['database']), "\">Show All Views</a> |\n";
+		echo "<a class=navlink href=\"$PHP_SELF?action=properties&database=", urlencode($_REQUEST['database']), "&view=", 
+			urlencode($_REQUEST['view']), "\">Properties</a></p>\n";
 	}
 	
 	/**
 	 * Show read only properties for a view
 	 */
 	function doProperties($msg = '') {
-		global $data, $localData, $misc, $database, $view;
+		global $data, $localData, $misc;
 		global $PHP_SELF, $strName, $strDefinition;
 	
-		echo "<h2>", htmlspecialchars($database), ": Views: ", htmlspecialchars($view), ": Properties</h2>\n";
+		echo "<h2>", htmlspecialchars($_REQUEST['database']), ": Views: ", htmlspecialchars($_REQUEST['view']), ": Properties</h2>\n";
 		$misc->printMsg($msg);
 		
-		$viewdata = &$localData->getView($view);
+		$viewdata = &$localData->getView($_REQUEST['view']);
 		
 		if ($viewdata->recordCount() > 0) {
 			echo "<table width=100%>\n";
@@ -82,32 +82,32 @@
 		}
 		else echo "<p>No data.</p>\n";
 		
-		echo "<p><a class=navlink href=\"$PHP_SELF?database=", urlencode($database), "\">Show All Views</a> |\n";
-		echo "<a class=navlink href=\"$PHP_SELF?action=edit&database=", urlencode($database), "&view=", 
-			urlencode($view), "\">Edit</a></p>\n";
+		echo "<p><a class=navlink href=\"$PHP_SELF?database=", urlencode($_REQUEST['database']), "\">Show All Views</a> |\n";
+		echo "<a class=navlink href=\"$PHP_SELF?action=edit&database=", urlencode($_REQUEST['database']), "&view=", 
+			urlencode($_REQUEST['view']), "\">Edit</a></p>\n";
 	}
 	
 	/**
 	 * Show confirmation of drop and perform actual drop
 	 */
 	function doDrop($confirm) {
-		global $localData, $database, $view;
+		global $localData, $database;
 		global $PHP_SELF;
 
 		if ($confirm) { 
-			echo "<h2>", htmlspecialchars($database), ": Views: ", htmlspecialchars($view), ": Drop</h2>\n";
+			echo "<h2>", htmlspecialchars($_REQUEST['database']), ": Views: ", htmlspecialchars($_REQUEST['view']), ": Drop</h2>\n";
 			
-			echo "<p>Are you sure you want to drop the view \"", htmlspecialchars($view), "\"?</p>\n";
+			echo "<p>Are you sure you want to drop the view \"", htmlspecialchars($_REQUEST['view']), "\"?</p>\n";
 			
 			echo "<form action=\"$PHP_SELF\" method=\"post\">\n";
 			echo "<input type=hidden name=action value=drop>\n";
-			echo "<input type=hidden name=view value=\"", htmlspecialchars($view), "\">\n";
-			echo "<input type=hidden name=database value=\"", htmlspecialchars($database), "\">\n";
+			echo "<input type=hidden name=view value=\"", htmlspecialchars($_REQUEST['view']), "\">\n";
+			echo "<input type=hidden name=database value=\"", htmlspecialchars($_REQUEST['database']), "\">\n";
 			echo "<input type=submit name=choice value=\"Yes\"> <input type=submit name=choice value=\"No\">\n";
 			echo "</form>\n";
 		}
 		else {
-			$status = $localData->dropView($view);
+			$status = $localData->dropView($_POST['view']);
 			if ($status == 0)
 				doDefault('View dropped.');
 			else
@@ -120,44 +120,48 @@
 	 * Displays a screen where they can enter a new view
 	 */
 	function doCreate($msg = '') {
-		global $data, $localData, $misc, $database, $view;
+		global $data, $localData, $misc;
 		global $PHP_SELF, $strName, $strDefinition;
-		global $formView, $formDefinition;
 		
-		if (!isset($formView)) $formView = '';
-		if (!isset($formDefinition)) $formDefinition = '';
+		if (!isset($_POST['formView'])) $_POST['formView'] = '';
+		if (!isset($_POST['formDefinition'])) $_POST['formDefinition'] = '';
 		
-		echo "<h2>", htmlspecialchars($database), ": Views: Create View</h2>\n";
+		echo "<h2>", htmlspecialchars($_REQUEST['database']), ": Views: Create View</h2>\n";
 		$misc->printMsg($msg);
 		
 		echo "<form action=\"$PHP_SELF\" method=post>\n";
 		echo "<table width=100%>\n";
 		echo "<tr><th class=data>{$strName}</th></tr>\n";
 		echo "<tr><td class=data1><input name=formView size={$data->_maxNameLen} maxlength={$data->_maxNameLen} value=\"", 
-			htmlspecialchars($formView), "\"></td></tr>\n";
+			htmlspecialchars($_POST['formView']), "\"></td></tr>\n";
 		echo "<tr><th class=data>{$strDefinition}</th></tr>\n";
 		echo "<tr><td class=data1><textarea style=\"width:100%;\" rows=20 cols=50 name=formDefinition wrap=virtual>", 
-			htmlspecialchars($formDefinition), "</textarea></td></tr>\n";
+			htmlspecialchars($_POST['formDefinition']), "</textarea></td></tr>\n";
 		echo "</table>\n";
 		echo "<input type=hidden name=action value=save_create>\n";
-		echo "<input type=hidden name=database value=\"", htmlspecialchars($database), "\">\n";
+		echo "<input type=hidden name=database value=\"", htmlspecialchars($_REQUEST['database']), "\">\n";
 		echo "<input type=submit value=Save> <input type=reset>\n";
 		echo "</form>\n";
 		
-		echo "<p><a class=navlink href=\"$PHP_SELF?database=", urlencode($database), "\">Show All Views</a></p>\n";
+		echo "<p><a class=navlink href=\"$PHP_SELF?database=", urlencode($_REQUEST['database']), "\">Show All Views</a></p>\n";
 	}
 	
 	/**
 	 * Actually creates the new view in the database
 	 */
 	function doSaveCreate() {
-		global $localData, $formView, $formDefinition;
+		global $localData, $strViewNeedsName, $strViewNeedsDef;
 		
-		$status = $localData->createView($formView, $formDefinition);
-		if ($status == 0)
-			doDefault('View created.');
-		else
-			doCreate('View creation failed.');
+		// Check that they've given a name and a definition
+		if ($_POST['formView'] == '') doCreate($strViewNeedsName);
+		elseif ($_POST['formDefinition'] == '') doCreate($strViewNeedsDef);
+		else {		 
+			$status = $localData->createView($_POST['formView'], $_POST['formDefinition']);
+			if ($status == 0)
+				doDefault('View created.');
+			else
+				doCreate('View creation failed.');
+		}
 	}	
 
 	/**
@@ -167,7 +171,7 @@
 		global $data, $localData, $misc, $database, $view;
 		global $PHP_SELF, $strView, $strOwner, $strActions, $strNoViews;
 		
-		echo "<h2>", htmlspecialchars($database), ": Views</h2>\n";
+		echo "<h2>", htmlspecialchars($_REQUEST['database']), ": Views</h2>\n";
 		$misc->printMsg($msg);
 		
 		$views = &$localData->getViews();
@@ -181,12 +185,12 @@
 				echo "<tr><td class=data{$id}>", htmlspecialchars($views->f[$data->vwFields['vwname']]), "</td>\n";
 				echo "<td class=data{$id}>", htmlspecialchars($views->f[$data->vwFields['vwowner']]), "</td>\n";
 				echo "<td class=opbutton{$id}><a href=\"$PHP_SELF?action=browse&offset=0&limit=30&database=", 
-					htmlspecialchars($database), "&table=", urlencode($views->f[$data->vwFields['vwname']]), "\">Browse</a></td>\n";
+					htmlspecialchars($_REQUEST['database']), "&view=", urlencode($views->f[$data->vwFields['vwname']]), "\">Browse</a></td>\n";
 				echo "<td class=opbutton{$id}>Select</td>\n";
 				echo "<td class=opbutton{$id}><a href=\"$PHP_SELF?action=properties&database=", 
-					htmlspecialchars($database), "&view=", urlencode($views->f[$data->vwFields['vwname']]), "\">Properties</a></td>\n";
+					htmlspecialchars($_REQUEST['database']), "&view=", urlencode($views->f[$data->vwFields['vwname']]), "\">Properties</a></td>\n";
 				echo "<td class=opbutton{$id}><a href=\"$PHP_SELF?action=confirm_drop&database=", 
-					htmlspecialchars($database), "&view=", urlencode($views->f[$data->vwFields['vwname']]), "\">Drop</a></td>\n";
+					htmlspecialchars($_REQUEST['database']), "&view=", urlencode($views->f[$data->vwFields['vwname']]), "\">Drop</a></td>\n";
 				echo "</tr>\n";
 				$views->moveNext();
 				$i++;
@@ -197,7 +201,7 @@
 			echo "<p>{$strNoViews}</p>\n";
 		}
 		
-		echo "<p><a class=navlink href=\"$PHP_SELF?action=create&database=", urlencode($database), "\">Create View</a></p>\n";
+		echo "<p><a class=navlink href=\"$PHP_SELF?action=create&database=", urlencode($_REQUEST['database']), "\">Create View</a></p>\n";
 
 	}
 
@@ -212,7 +216,7 @@
 			doCreate();
 			break;
 		case 'drop':
-			if ($choice == 'Yes') doDrop(false);
+			if ($_POST['choice'] == 'Yes') doDrop(false);
 			else doDefault();
 			break;
 		case 'confirm_drop':
