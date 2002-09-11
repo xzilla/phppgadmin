48da7c4ee60c1eef1b07410c8df898021cba1a8a
xzilla <xzilla>
2002-09-11 15:20:24 +0000

    I did half of the spanish translation just to test language functionality

---
+++ lang/spanish.php
@@ -0,0 +1,59 @@
+<?php
+
+	/**
+	 * Language template file for WebDB.  Use this to base language
+	 * files.
+	 *
+	 * $Id: spanish.php,v 1.1 2002/09/11 15:20:24 xzilla Exp $
+	 */
+
+	$appLang = 'spanish';
+
+-	$strNoFrames = 'You need a frames-enabled browser to use this application.';
+-	$strLogin = 'Login';
+	$strNoTables = "No se han encontrado tablas.";
+-	$strNoViews = 'No views found.';
+	$strNoFunctions = 'No se han encontrado funci&oacute;nes.';
+-	$strOwner = 'Owner';
+	$strActions = "Acci&oacute;nes";
+	$strName = 'Nombre';
+	$strTable = "Tabla ";
+	$strTables = 'Tablas';
+-	$strView = 'View';
+-	$strViews = 'Views';
+-	$strDefinition = 'Definition';
+-	$strTriggers = 'Triggers';
+-	$strRules = 'Rules';
+-	$strSequences = 'Sequences';
+	$strFunction = "Funci&oacute;n";
+	$strFunctions = "Funci&oacute;nes";
+-	$strOperators = 'Operators';
+	$strTypes = 'Tipos';
+-	$strAggregates = 'Aggregates';
+	
+	// Users
+	$strUsername = 'Usario';
+	$strPassword = 'Password';
+-	$strSuper = 'Superuser?';
+-	$strCreateDB = 'Create DB?';
+-	$strExpires = 'Expires';	
+-	$strNoUsers = 'No users found.';
+	
+	// Views
+-	$strViewNeedsName = 'You must give a name for your view.';
+-	$strViewNeedsDef = 'You must give a definition for your view.';
+
+	// Sequences
+-	$strNoSequences = 'No sequences found.';
+	
+	// Tables
+	$strField = "Campo";
+	$strType = "Tipo";
+	$strValue = "Valor";
+
+	// Functions
+-	$strReturns = 'Returns';
+-	$strArguments = 'Arguments';
+-	$strLanguage = 'Language';
+
+?>
