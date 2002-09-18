97846bc6771fb6526892f6cf26c4e52250d089f0
xzilla <xzilla>
2002-09-18 17:33:01 +0000

    I got tired of getting an error message when I clicked on a database name. I think we should put something more descriptive in here, but this will work for now.

---
+++ public_html/database.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * List databases in a server
+	 * @param $webdbServerID The ID of the current server
+	 *
+	 * $Id: database.php,v 1.1 2002/09/18 17:33:01 xzilla Exp $
+	 */
+
+	// Include application functions
+	include_once('../conf/config.inc.php');
+
+?>
+
+<html>
+<body>
+
+<h2><?= $appName ?> :: <?= $_GET['database'] ?></h2>
+
+
+</body>
+</html>
