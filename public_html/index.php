368f121ad4dacddf15f1e3e861c9ed926cb98992
xzilla <xzilla>
2002-10-02 04:41:38 +0000

    Config needs to be before html, which breaks xhtml compliance by moving style to wrong place

--- public_html/index.php
+++ public_html/index.php
@@ -3,20 +3,16 @@
 	/**
 	 * Main access point to WebDB.
 	 *
-	 * $Id: index.php,v 1.4 2002/10/02 04:35:51 xzilla Exp $
+	 * $Id: index.php,v 1.5 2002/10/02 04:41:38 xzilla Exp $
 	 */
 
+	// Include application functions
+	include_once('../conf/config.inc.php');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<?php
-
-	// Include application functions
-	include_once('../conf/config.inc.php');
-
-?>
 <title><?= $appName ?></title>
 </head>
 
