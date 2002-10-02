2b210de42a2ac7c6daafc43df46a5c17bea3c42c
xzilla <xzilla>
2002-10-02 04:35:51 +0000

    make index.php xhtml compliant

--- public_html/index.php
+++ public_html/index.php
@@ -3,29 +3,33 @@
 	/**
 	 * Main access point to WebDB.
 	 *
-	 * $Id: index.php,v 1.3 2002/04/10 04:09:47 chriskl Exp $
+	 * $Id: index.php,v 1.4 2002/10/02 04:35:51 xzilla Exp $
 	 */
+
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Frameset//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-frameset.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<?php
 
 	// Include application functions
 	include_once('../conf/config.inc.php');
 
 ?>
-
-<html>
-<head>
 <title><?= $appName ?></title>
 </head>
 
-<frameset rows="50, *" border="0" frameborder="0" framespacing="0">
-	<frame src="topbar.php" name="topbar">
-	<frameset cols="<?= $guiLeftFrameWidth ?>,*" border="0" frameborder="0">
-	  <frame src="browser.php" name="browser">
-	  <frame src="intro.php" name="detail">
+<frameset rows="50, *">
+	<frame src="topbar.php" name="topbar" />
+	<frameset cols="<?= $guiLeftFrameWidth ?>,*">
+	  <frame src="browser.php" name="browser" />
+	  <frame src="intro.php" name="detail" />
 	</frameset>
+	<noframes>
+	<body>
+		<?= $strNoFrames ?>
+	</body>
+	</noframes>
 </frameset>
-<noframes>
-<body>
-	<?= $strNoFrames ?>
-</body>
-</noframes>
 </html>
