d52eed6f06adc79d577601c8ca68d57eea639751
xzilla <xzilla>
2002-11-09 15:59:40 +0000

    initial german translation thanks to Albin Blaschka <blaschka@unix.sbg.ac.at>

---
+++ lang/german.php
@@ -0,0 +1,101 @@
+<?php
+
+	/**
+	 * German Language file for WebDB.
+	 *
+	 * $Id: german.php,v 1.1 2002/11/09 15:59:40 xzilla Exp $
+	 */
+
+	$appLang = 'deutsch';
+
+	$strNoFrames = 'F&uuml;r dieses Programm wird ein ein Frame-fähiger Browser ben&ouml;tigt';
+    $strLogin = 'Login';
+	$strNoTables = 'Keine Tabellen vorhanden';
+	$strNoTable = 'Keine Tabelle vorhanden';
+	$strNoViews = 'Keine Views vorhanden';
+	$strNoFunctions = 'Keine Funktionen vorhanden';
+	$strOwner = 'Eigent&uuml;mer';
+	$strAction = 'Action';	
+	$strActions = 'Actions';	
+	$strName = 'Name';
+	$strTable = 'Tabelle';
+	$strTables = 'Tabellen';
+	$strView = 'View';
+	$strViews = 'Views';
+	$strDefinition = 'Definition';
+	$strTriggers = 'Trigger';
+	$strRules = 'Rules';
+	$strSequence = 'Sequenz';
+	$strSequences = 'Sequenzen';
+	$strFunction = 'Funktion';
+	$strFunctions = 'Funktionen';
+	$strOperators = 'Operatoren';
+	$strTypes = 'Typen';
+	$strAggregates = 'Aggregate';
+	$strIndicies = 'Indicies';
+	$strProperties = 'Eigenschaften';
+	$strBrowse = 'Suchen';
+	$strDrop = 'L&ouml;schen';
+	$strDropped = 'Gel&ouml;scht';
+	$strNull = 'Null';
+	$strPrev = 'Vorhergehende';
+	$strNext = 'Nächste';
+	$strFailed = 'Fehler';
+	$strNotLoaded = 'Ihre PHP-Installation besitzt keine Datenbank-Unterst&uuml;tzung';
+	$strCreate = 'Erzeugen';
+	$strComment = 'Kommentar';
+	
+	// Users
+	$strUsername = 'Benutzername';
+	$strPassword = 'Passwort';
+	$strSuper = 'Superuser?';
+	$strCreateDB = 'Erzeuge DB?';
+	$strExpires = 'L&auml;ft ab';
+	$strNoUsers = 'Keine Benutzer vorhanden';
+	
+	// Databases
+	$strDatabase = 'Datenbank';
+	$strDatabases = 'Datenbanken';
+	$strNoDatabases = 'Keine Datenbanken vorhanden';
+	$strDatabaseNeedsName = 'Sie m&uuml;ssen der Datenbank einen Namen geben';
+	
+	// Views
+	$strViewNeedsName = 'Sie m&uuml;ssen der View einen Namen geben';
+	$strViewNeedsDef = 'Sie m&uuml;ssen die View definieren';
+
+	// Sequences
+	$strNoSequences = 'Keine Sequenzen gefunden';
+	$strSequenceName = 'sequence_name';
+	$strLastValue = 'last_value';
+	$strIncrementBy = 'increment_by';	
+	$strMaxValue = 'max_value';
+	$strMinValue = 'min_value';
+	$strCacheValue = 'cache_value';
+	$strLogCount = 'log_cnt';
+	$strIsCycled = 'is_cycled';
+	$strIsCalled = 'is_called';
+	$strReset =	'Zur&uuml;cksetzen';
+
+	// Indicies
+	$strIndexName = 'Index Name';
+	$strTabName = 'Tab Name';
+	$strColumnName = 'Spaltenname';
+	$strUniqueKey = 'Unique Key';
+	$strPrimaryKey = 'Prim&auml;rschl&uuml;ssel';
+	
+	// Tables
+	$strField = 'Feld';
+	$strType = 'Type';
+	$strValue = 'Wert';
+	$strShowAllTables = 'Alle Tabellen anzeigen';
+	$strUnique = 'Unique';
+	$strPrimary = 'Primary';
+	$strKeyName = 'Key Name';
+
+	// Functions
+	$strReturns = 'Liefert';
+	$strArguments = 'Argumente';
+	$strLanguage = 'Sprache';
+	$strFunctionNeedsName = 'Sie m&uuml;ssen der Funktion einen Namen geben';
+	$strFunctionNeedsDef = 'Sie m&uuml;ssen die Funktion definieren';
+?>
