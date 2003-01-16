fa18f7d912149175d420ad141aa0de25d07724fb
jmpoure <jmpoure>
2003-01-16 09:01:46 +0000

    Initial upload

---
+++ lang/chinese-tr-unicodehtml.php
@@ -0,0 +1,194 @@
+&lt;?php
+
+	/**
+	 * Chinese language file.  UTF-8 encoded.
+	 * @maintainer Chan Min Wai [dcmwai@amtb-m.org.my]
+	 *
+	 * $Id: chinese-tr-unicodehtml.php,v 1.1 2003/01/16 09:01:46 jmpoure Exp $
+	 */
+
+	$appLang = '&#32321;&#39636;&#20013;&#25991;&#65288;&#33836;&#22283;&#30908;&#65289;';
+	$appCharset = 'UTF-8';
+
+	$strIntro = '&#27489;&#36814;&#20351;&#29992; WebDB&#12290;';
+	$strNoFrames = '&#24744;&#24517;&#38920;&#20351;&#29992;&#26377;&#26694;&#26550;&#32080;&#27083;&#30340;&#28687;&#35261;&#22120;&#20197;&#22519;&#34892;&#26412;&#31243;&#24335;&#12290;';
+	$strBadConfig = '&#24744;&#30340; config.inc.php &#24050;&#32147;&#19981;&#36969;&#29992;&#20102;&#12290;&#24744;&#38656;&#35201;&#33258;&#34892;&#30340;&#30001; config.inc.php-dist &#36914;&#34892;&#20462;&#25913;&#12290;';
+	$strLogin = '&#30331;&#20837;';
+	$strLoginFailed = '&#30331;&#20837;&#22833;&#25943;';
+	$strServer = '&#20282;&#26381;&#22120;';
+	$strNoTables = '&#36039;&#26009;&#34920;&#19981;&#23384;&#22312;&#12290;';
+	$strNoTable = '&#36039;&#26009;&#34920;&#19981;&#23384;&#22312;&#12290;';
+	$strNoViews = '&#35264;&#30475;&#19981;&#23384;&#22312;&#12290;';
+	$strNoFunctions = '&#21151;&#33021;&#19981;&#23384;&#22312;&#12290;';
+	$strOwner = '&#25152;&#26377;&#20154;';
+	$strAction = 'Action';	
+	$strActions = 'Actions';	
+	$strName = '&#21517;&#23383;';
+	$strTable = '&#36039;&#26009;&#34920;';
+	$strTables = '&#36039;&#26009;&#34920;';
+	$strView = '&#35264;&#30475;';
+	$strViews = '&#35264;&#30475;';
+	$strDefinition = '&#35299;&#35498;';
+	$strRules = '&#35215;&#21063;';
+	$strSequence = '&#27425;&#24207;';
+	$strSequences = '&#27425;&#24207;';
+	$strFunction = '&#21151;&#33021;';
+	$strFunctions = '&#21151;&#33021;';
+	$strOperators = '&#25805;&#20316;&#21729;';
+	$strTypes = '&#39006;&#22411;';
+	$strAggregates = 'Aggregates';
+	$strIndicies = '&#32034;&#24341;';
+	$strProperties = '&#20839;&#23481;';
+	$strBrowse = '&#28687;&#35261;';
+	$strDrop = '&#19999;&#26820;';
+	$strDropped = '&#24050;&#19999;&#26820;';
+	$strNull = 'Null';
+	$strNotNull = 'Not Null';
+	$strPrev = '&#21069;&#19968;&#31558;';
+	$strNext = '&#19979;&#19968;&#31558;';
+	$strFailed = '&#22833;&#25943;';
+	$strNotLoaded = '&#24744;&#30340; PHP &#20013;&#20006;&#28961;&#23436;&#25972;&#30340;&#36039;&#26009;&#24235;&#23383;&#25903;&#25588;&#12290;';
+	$strCreate = '&#26032;&#22686;';
+	$strComment = '&#27880;&#37323;|';
+	//$strNext = 'Next'; (Double)
+	$strLength = '&#38263;&#24230;';
+	$strDefault = '&#38928;&#32622;&#20540;';
+	$strAlter = '&#26356;&#25913;';
+	$strCancel = '&#21462;&#28040;';
+	$strSave = '&#20786;&#33988;';
+	$strInsert = '&#25554;&#20837;';
+	$strSelect = '&#36984;&#25799;';
+	$strDelete = '&#21034;&#38500;';
+	$strUpdate = '&#26356;&#26032;';
+	$strRule = '&#35215;&#21063;';
+	$strReferences = '&#21443;&#32771;';
+	$strYes = '&#26159;';
+	$strNo = '&#21542;';
+	$strEdit = '&#32232;&#36655;';
+	$strInvalidParam = 'Invalid script parameters.';
+	$strRows = '&#34892;';
+	$strExample = '&#22914;&#65306;';
+	
+	// Error handling
+	$strSQLError = 'SQL &#37679;&#35492;:';
+	$strInStatement = 'In statement:';
+	
+	// Users
+	$strUser = '&#20351;&#29992;&#32773;';
+	$strGroup = '&#32676;&#32068;';
+	$strUsername = '&#29992;&#25142;&#21517;&#31281;';
+	$strPassword = '&#23494;&#30908;';
+	$strSuper = '&#36229;&#32026;&#29992;&#25142;&#65311;';
+	$strCreateDB = '&#26032;&#22686;&#36039;&#26009;&#24235;&#65311;';
+	$strExpires = '&#36942;&#26399;';	
+	$strNoUsers = '&#26597;&#28961;&#27492;&#20351;&#29992;&#32773;';
+	
+	// Privilges
+	$strPrivileges = '&#29305;&#27402;';
+	$strGrant = '&#28310;&#20104;';
+	$strRevoke = '&#25764;&#22238;';
+
+	// Databases
+	$strDatabase = '&#36039;&#26009;&#24235;';
+	$strDatabases = '&#36039;&#26009;&#24235;';
+	$strNoDatabases = '&#26597;&#28961;&#27492;&#36039;&#26009;&#24235;&#12290;';
+	$strDatabaseNeedsName = '&#24744;&#24517;&#38920;&#32102;&#36039;&#26009;&#24235;&#19968;&#22266;&#21517;&#31281;';
+	
+	// Views
+	$strViewNeedsName = '&#24744;&#30340;&#35264;&#30475;&#24517;&#38920;&#26377;&#19968;&#20491;&#21517;&#31281;&#12290;';
+	$strViewNeedsDef = '&#24744;&#30340;&#35264;&#30475;&#24517;&#38920;&#26377;&#19968;&#20491;&#35299;&#35498;&#12290;';
+
+	// Sequences
+	$strNoSequences = '&#26597;&#28961;&#27425;&#24207;&#12290;';
+	$strSequenceName = '&#27425;&#24207;&#21517;&#31281;';
+	$strLastValue = '&#26368;&#24460;&#30340;&#25976;&#30446;';
+	$strIncrementBy = '&#22686;&#37327;&#65288;&#21152;&#65295;&#28187;&#65289;';	
+	$strMaxValue = '&#26368;&#22823;&#20540;';
+	$strMinValue = '&#26368;&#23567;&#20540;';
+	$strCacheValue = 'cache_value';
+	$strLogCount = 'log_cnt';
+	$strIsCycled = 'is_cycled';
+	$strIsCalled = 'is_called';
+	$strReset =	'&#37325;&#35373;';
+
+	// Indicies
+	$strIndexName = '&#32034;&#24341;&#21517;&#31281;';
+	$strTabName = 'Tab Name';
+	$strColumnName = '&#34892;&#21015;&#21517;&#31281;';
+	$strUniqueKey = '&#21807;&#19968;';
+	$strPrimaryKey = '&#20027;&#37749;';
+	$strShowAllIndicies = 'Show All Indicies';
+	$strCreateIndex = 'Create Index';
+	$strIndexNeedsName = 'You must give a name for your index';
+	$strIndexNeedsCols = 'You must give a valid number of columns.';
+	$strIndexCreated = 'Index created';
+	$strIndexCreatedBad = 'Index creation failed.';
+	$strConfDropIndex = 'Are you sure you want to drop the index &quot;%s&quot;?';
+	$strIndexDropped = 'Index dropped.';
+	$strIndexDroppedBad = 'Index drop failed.';
+	
+	// Tables
+	$strField = 'Field';
+	$strFields = 'Fields';
+	$strType = 'Type';
+	$strValue = 'Value';
+	$strShowAllTables = 'Show All Tables';
+	$strUnique = 'Unique';
+	$strPrimary = 'Primary';
+	$strKeyName = 'Key Name';
+	$strNumFields = 'Num. Of Fields';
+	$strCreateTable = 'Create Table';
+	$strTableNeedsName = 'You must give a name for your table.';
+	$strTableNeedsCols = 'You must give a valid number of columns.';
+	$strExport = 'Export';
+	$strConstraints = 'Constraints';
+	$strColumns = 'Columns';
+	
+	// Functions
+	$strReturns = 'Returns';
+	$strArguments = 'Arguments';
+	$strLanguage = 'Language';
+	$strFunctionNeedsName = 'You must give a name for your function.';
+	$strFunctionNeedsDef = 'You must give a definition for your function.';
+	
+	// Triggers
+	$strTrigger = 'Trigger';
+	$strTriggers = 'Triggers';
+	$strNoTriggers = 'No triggers found.';
+	$strCreateTrigger = 'Create Trigger';
+	
+	// Types
+	$strType = 'Type';
+	$strTypes = 'Types';
+	$strNoTypes = 'No types found.';
+	$strCreateType = 'Create Type';
+	$strConfDropType = 'Are you sure you want to drop the type &quot;%s&quot;?';
+	$strTypeDropped = 'Type dropped.';
+	$strTypeDroppedBad = 'Type drop failed.';
+	$strTypeCreated = 'Type created';
+	$strTypeCreatedBad = 'Type creation failed.';
+	$strShowAllTypes = 'Show all types';
+	$strInputFn = 'Input function';
+	$strOutputFn = 'Output function';
+	$strPassByVal = 'Passed by val?';
+	$strAlignment = 'Alignment';
+	$strElement = 'Element';
+	$strDelimiter = 'Delimiter';
+	$strStorage = 'Storage';
+	$strTypeNeedsName = 'You must give a name for your type.';
+	$strTypeNeedsLen = 'You must give a length for your type.';
+
+	// Schemas
+	$strSchema = 'Schema';
+	$strSchemas = 'Schemas';
+	$strCreateSchema = 'Create Schema';
+	$strNoSchemas = 'No schemas found.';
+	$strConfDropSchema = 'Are you sure you want to drop the schema &quot;%s&quot;?';
+	$strSchemaDropped = 'Schema dropped.';
+	$strSchemaDroppedBad = 'Schema drop failed.';
+	$strSchemaCreated = 'Schema created';
+	$strSchemaCreatedBad = 'Schema creation failed.';
+	$strShowAllSchemas = 'Show All Schemas';
+	$strSchemaNeedsName = 'You must give a name for your schema.';
+
+?&gt;
