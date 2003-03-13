fd29c6b6b7cccc4c31c55a9fae3aac01eb0c1e6b
chriskl <chriskl>
2003-03-13 01:49:20 +0000

    fix totally stuffed spanish file

--- lang/recoded/spanish.php
+++ lang/recoded/spanish.php
@@ -1,51 +1,51 @@
 <?php
 
 	/**
-	 * Spanish language file for WebDB.
+	 * Spanish language file for phpPgAdmin.
 	 * @maintainer Robert Treat [xzilla@users.sourceforge.net]
 	 *
-	 * $Id: spanish.php,v 1.1 2003/01/18 08:23:24 chriskl Exp $
+	 * $Id: spanish.php,v 1.2 2003/03/13 01:49:20 chriskl Exp $
 	 */
 
 	$appLang = 'Spanish';
 	$appCharset = 'ISO-8859-1';
 
--	$strNoFrames = 'You need a frames-enabled browser to use this application.';
--	$strLogin = 'Login';
+	$strNoFrames = 'You need a frames-enabled browser to use this application.';
+	$strLogin = 'Login';
 	$strNoTables = &quot;No se han encontrado tablas.&quot;;
--	$strNoViews = 'No views found.';
+	$strNoViews = 'No views found.';
 	$strNoFunctions = 'No se han encontrado funci&amp;oacute;nes.';
--	$strOwner = 'Owner';
+	$strOwner = 'Owner';
 	$strActions = &quot;Acci&amp;oacute;nes&quot;;
 	$strName = 'Nombre';
 	$strTable = &quot;Tabla &quot;;
 	$strTables = 'Tablas';
--	$strView = 'View';
--	$strViews = 'Views';
--	$strDefinition = 'Definition';
--	$strTriggers = 'Triggers';
--	$strRules = 'Rules';
--	$strSequences = 'Sequences';
+	$strView = 'View';
+	$strViews = 'Views';
+	$strDefinition = 'Definition';
+	$strTriggers = 'Triggers';
+	$strRules = 'Rules';
+	$strSequences = 'Sequences';
 	$strFunction = &quot;Funci&amp;oacute;n&quot;;
 	$strFunctions = &quot;Funci&amp;oacute;nes&quot;;
--	$strOperators = 'Operators';
+	$strOperators = 'Operators';
 	$strTypes = 'Tipos';
--	$strAggregates = 'Aggregates';
+	$strAggregates = 'Aggregates';
 	
 	// Users
 	$strUsername = 'Usario';
 	$strPassword = 'Password';
--	$strSuper = 'Superuser?';
--	$strCreateDB = 'Create DB?';
--	$strExpires = 'Expires';	
--	$strNoUsers = 'No users found.';
+	$strSuper = 'Superuser?';
+	$strCreateDB = 'Create DB?';
+	$strExpires = 'Expires';	
+	$strNoUsers = 'No users found.';
 	
 	// Views
--	$strViewNeedsName = 'You must give a name for your view.';
--	$strViewNeedsDef = 'You must give a definition for your view.';
+	$strViewNeedsName = 'You must give a name for your view.';
+	$strViewNeedsDef = 'You must give a definition for your view.';
 
 	// Sequences
--	$strNoSequences = 'No sequences found.';
+	$strNoSequences = 'No sequences found.';
 	
 	// Tables
 	$strField = &quot;Campo&quot;;
@@ -53,8 +53,8 @@
 	$strValue = &quot;Valor&quot;;
 
 	// Functions
--	$strReturns = 'Returns';
--	$strArguments = 'Arguments';
--	$strLanguage = 'Language';
+	$strReturns = 'Returns';
+	$strArguments = 'Arguments';
+	$strLanguage = 'Language';
 
 ?>
