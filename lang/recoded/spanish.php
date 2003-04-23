0a8c36e503bfc21529c6e6053f7e50b4265246bc
chriskl <chriskl>
2003-04-23 15:13:13 +0000

    rerun of recode to fix encoding problems

--- lang/recoded/spanish.php
+++ lang/recoded/spanish.php
@@ -4,19 +4,20 @@
 	 * Spanish language file for phpPgAdmin.
 	 * @maintainer Robert Treat [xzilla@users.sourceforge.net]
 	 *
-	 * $Id: spanish.php,v 1.4 2003/03/17 05:20:31 chriskl Exp $
+	 * $Id: spanish.php,v 1.5 2003/04/23 15:13:13 chriskl Exp $
 	 */
 
 	$lang['applang'] = 'Spanish';
 	$lang['appcharset'] = 'ISO-8859-1';
-
+	$lang['applocale'] = 'es_ES';
+  
 	$lang['strnoframes'] = 'You need a frames-enabled browser to use this application.';
 	$lang['strlogin'] = 'Login';
 	$lang['strnotables'] = 'No se han encontrado tablas.';
 	$lang['strnoviews'] = 'No views found.';
-	$lang['strnofunctions'] = 'No se han encontrado funci&amp;oacute;nes.';
+	$lang['strnofunctions'] = 'No se han encontrado funci&oacute;nes.';
 	$lang['strowner'] = 'Owner';
-	$lang['stractions'] = 'Acci&amp;oacute;nes';
+	$lang['stractions'] = 'Acci&oacute;nes';
 	$lang['strname'] = 'Nombre';
 	$lang['strtable'] = 'Tabla ';
 	$lang['strtables'] = 'Tablas';
@@ -26,8 +27,8 @@
 	$lang['strtriggers'] = 'Triggers';
 	$lang['strrules'] = 'Rules';
 	$lang['strsequences'] = 'Sequences';
-	$lang['strfunction'] = 'Funci&amp;oacute;n';
-	$lang['strfunctions'] = 'Funci&amp;oacute;nes';
+	$lang['strfunction'] = 'Funci&oacute;n';
+	$lang['strfunctions'] = 'Funci&oacute;nes';
 	$lang['stroperators'] = 'Operators';
 	$lang['strtypes'] = 'Tipos';
 	$lang['straggregates'] = 'Aggregates';
