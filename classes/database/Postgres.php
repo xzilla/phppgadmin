0280e5f3efcba170c74947d5fd48e6dfb12c0ec5
chriskl <chriskl>
2003-05-08 15:25:58 +0000

    don't list primary and unique indexes under indexes - list them in constraints

--- classes/database/Postgres.php
+++ classes/database/Postgres.php
@@ -4,7 +4,7 @@
  * A class that implements the DB interface for Postgres
  * Note: This class uses ADODB and returns RecordSets.
  *
- * $Id: Postgres.php,v 1.98 2003/05/08 15:14:14 chriskl Exp $
+ * $Id: Postgres.php,v 1.99 2003/05/08 15:25:58 chriskl Exp $
  */
 
 // @@@ THOUGHT: What about inherits? ie. use of ONLY???
@@ -1204,6 +1204,7 @@ class Postgres extends BaseDB {
 		$sql = "SELECT c2.relname, i.indisprimary, i.indisunique, pg_get_indexdef(i.indexrelid)
 			FROM pg_class c, pg_class c2, pg_index i
 			WHERE c.relname = '{$table}' AND c.oid = i.indrelid AND i.indexrelid = c2.oid
+			AND NOT i.indisprimary AND NOT i.indisunique
 			ORDER BY c2.relname";
 
 		return $this->selectSet($sql);
