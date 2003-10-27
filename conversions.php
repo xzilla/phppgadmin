650c3892536691c3ea140cf094e0768edbcbe71f
chriskl <chriskl>
2003-10-27 06:37:25 +0000

    oops - forgot to actually add the conversions and languages files

---
+++ conversions.php
@@ -0,0 +1,63 @@
+<?php
+
+	/**
+	 * Manage conversions in a database
+	 *
+	 * $Id: conversions.php,v 1.1 2003/10/27 06:37:25 chriskl Exp $
+	 */
+
+	// Include application functions
+	include_once('libraries/lib.inc.php');
+	
+	$action = (isset($_REQUEST['action'])) ? $_REQUEST['action'] : '';
+	if (!isset($msg)) $msg = '';
+	$PHP_SELF = $_SERVER['PHP_SELF'];
+
+	/**
+	 * Show default list of conversions in the database
+	 */
+	function doDefault($msg = '') {
+		global $data, $localData, $misc, $database;
+		global $PHP_SELF, $lang;
+
+		echo "<h2>", $misc->printVal($_REQUEST['database']), ": {$lang['strconversions']}</h2>\n";
+		$misc->printMsg($msg);
+		
+		$conversions = &$localData->getconversions();
+
+		if ($conversions->recordCount() > 0) {
+			echo "<table>\n";
+			echo "<tr><th class=\"data\">{$lang['strname']}</th><th class=\"data\">{$lang['strsourceencoding']}</th>";
+			echo "<th class=\"data\">{$lang['strtargetencoding']}</th><th class=\"data\">{$lang['strdefault']}</th>";
+			echo "</tr>\n";
+			$i = 0;
+			while (!$conversions->EOF) {
+				$conversions->f['condefault'] = $data->phpBool($conversions->f['condefault']);
+				$id = (($i % 2) == 0 ? '1' : '2');
+				echo "<tr><td class=\"data{$id}\">", $misc->printVal($conversions->f['conname']), "</td>\n";
+				echo "<td class=\"data{$id}\">", $misc->printVal($conversions->f['conforencoding']), "</td>\n";
+				echo "<td class=\"data{$id}\">", $misc->printVal($conversions->f['contoencoding']), "</td>\n";
+				echo "<td class=\"data{$id}\">", ($conversions->f['condefault']) ? $lang['stryes'] : $lang['strno'], "</td>\n";
+				echo "</tr>\n";
+				$conversions->moveNext();
+				$i++;
+			}
+			echo "</table>\n";
+		}
+		else {
+			echo "<p>{$lang['strnoconversions']}</p>\n";
+		}
+	}
+
+	$misc->printHeader($lang['strconversions']);
+	$misc->printBody();
+
+	switch ($action) {
+		default:
+			doDefault();
+			break;
+	}	
+
+	$misc->printFooter();
+
+?>
---
+++ languages.php
@@ -0,0 +1,62 @@
+<?php
+
+	/**
+	 * Manage languages in a database
+	 *
+	 * $Id: languages.php,v 1.1 2003/10/27 06:37:25 chriskl Exp $
+	 */
+
+	// Include application functions
+	include_once('libraries/lib.inc.php');
+	
+	$action = (isset($_REQUEST['action'])) ? $_REQUEST['action'] : '';
+	if (!isset($msg)) $msg = '';
+	$PHP_SELF = $_SERVER['PHP_SELF'];
+
+	/**
+	 * Show default list of languages in the database
+	 */
+	function doDefault($msg = '') {
+		global $data, $localData, $misc, $database;
+		global $PHP_SELF, $lang;
+
+		echo "<h2>", $misc->printVal($_REQUEST['database']), ": {$lang['strlanguages']}</h2>\n";
+		$misc->printMsg($msg);
+		
+		$languages = &$localData->getlanguages();
+
+		if ($languages->recordCount() > 0) {
+			echo "<table>\n";
+			echo "<tr><th class=\"data\">{$lang['strname']}</th><th class=\"data\">{$lang['strtrusted']}</th>";
+			echo "<th class=\"data\">{$lang['strfunction']}</th>";
+			echo "</tr>\n";
+			$i = 0;
+			while (!$languages->EOF) {
+				$languages->f['lanpltrusted'] = $data->phpBool($languages->f['lanpltrusted']);
+				$id = (($i % 2) == 0 ? '1' : '2');
+				echo "<tr><td class=\"data{$id}\">", $misc->printVal($languages->f['lanname']), "</td>\n";
+				echo "<td class=\"data{$id}\">", ($languages->f['lanpltrusted']) ? $lang['stryes'] : $lang['strno'], "</td>\n";
+				echo "<td class=\"data{$id}\">", $misc->printVal($languages->f['lanplcallf']), "</td>\n";
+				echo "</tr>\n";
+				$languages->moveNext();
+				$i++;
+			}
+			echo "</table>\n";
+		}
+		else {
+			echo "<p>{$lang['strnolanguages']}</p>\n";
+		}
+	}
+
+	$misc->printHeader($lang['strlanguages']);
+	$misc->printBody();
+
+	switch ($action) {
+		default:
+			doDefault();
+			break;
+	}	
+
+	$misc->printFooter();
+
+?>
