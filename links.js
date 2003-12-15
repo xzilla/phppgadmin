0ffa5171a020e4b4473e1f5f6b482d2d65a2056a
chriskl <chriskl>
2003-12-15 08:14:10 +0000

    make clicking on a database update the detail view as well

--- links.js
+++ links.js
@@ -2,13 +2,15 @@
  * Function for updating browser frame and topbar frame so that sqledit window 
  * pops up with properly selected database.
  *
- * $Id: links.js,v 1.1 2003/12/15 07:59:22 chriskl Exp $
+ * $Id: links.js,v 1.2 2003/12/15 08:14:10 chriskl Exp $
  */
 function updateLinks(getVars) {
-	var topbarLink = 'topbar.php' + getVars
-	var browserLink = 'browser.php' + getVars
+	var topbarLink = 'topbar.php' + getVars;
+	var browserLink = 'browser.php' + getVars;
+	var detailLink = 'database.php' + getVars;
 		
 	parent.frames.topbar.location = topbarLink;
 	parent.frames.browser.location = browserLink;
+	parent.frames.detail.location = detailLink;
 }
 
