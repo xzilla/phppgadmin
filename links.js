19af5c8ff2944960e51780c6d7636466701c75c1
chriskl <chriskl>
2003-12-28 08:15:34 +0000

    improvement on frame reload order from brydan

--- links.js
+++ links.js
@@ -2,7 +2,7 @@
  * Function for updating browser frame and topbar frame so that sqledit window 
  * pops up with properly selected database.
  *
- * $Id: links.js,v 1.2 2003/12/15 08:14:10 chriskl Exp $
+ * $Id: links.js,v 1.3 2003/12/28 08:15:34 chriskl Exp $
  */
 function updateLinks(getVars) {
 	var topbarLink = 'topbar.php' + getVars;
@@ -10,7 +10,7 @@ function updateLinks(getVars) {
 	var detailLink = 'database.php' + getVars;
 		
 	parent.frames.topbar.location = topbarLink;
-	parent.frames.browser.location = browserLink;
 	parent.frames.detail.location = detailLink;
+	parent.frames.browser.location = browserLink;
 }
 
