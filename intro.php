9196a42f7e25b81c2a026b0784bfc2579d8efbb6
xzilla <xzilla>
2004-11-25 21:31:09 +0000

    add phppgadmin version to main page, along with php version. Fixes RFE 771166.

--- intro.php
+++ intro.php
@@ -3,7 +3,7 @@
 	/**
 	 * Intro screen
 	 *
-	 * $Id: intro.php,v 1.11 2004/09/23 12:06:46 soranzo Exp $
+	 * $Id: intro.php,v 1.12 2004/11/25 21:31:09 xzilla Exp $
 	 */
 
 	// Include application functions (no db conn)
@@ -14,7 +14,7 @@
 	$misc->printBody();
 ?>
 
-<h1><?php echo $appName ?></h1>
+<h1><?php echo "$appName $appVersion (PHP ". $phpversion() .')' ?></h1>
 
 <p><?php echo $lang['strintro'] ?></p>
 
