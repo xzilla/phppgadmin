b5cb5e0ee2bc9be6416524e8f5190e96eb73c908
jollytoad <jollytoad>
2005-03-03 14:35:13 +0000

    Updated to XLoadTree2(beta)

--- xloadtree/xmlextras.js
+++ xloadtree/xmlextras.js
@@ -1,4 +1,50 @@
-//<script>
+/*----------------------------------------------------------------------------\
+|                               XML Extras 1.01                               |
+|-----------------------------------------------------------------------------|
+|                         Created by Erik Arvidsson                           |
+|                  (http://webfx.eae.net/contact.html#erik)                   |
+|                      For WebFX (http://webfx.eae.net/)                      |
+|-----------------------------------------------------------------------------|
+| A library that makes working with XML documents in Mozilla more similar to  |
+| working with MSXML.                                                         |
+|-----------------------------------------------------------------------------|
+|                   Copyright (c) 1999 - 2003 Erik Arvidsson                  |
+|-----------------------------------------------------------------------------|
+| This software is provided "as is", without warranty of any kind, express or |
+| implied, including  but not limited  to the warranties of  merchantability, |
+| fitness for a particular purpose and noninfringement. In no event shall the |
+| authors or  copyright  holders be  liable for any claim,  damages or  other |
+| liability, whether  in an  action of  contract, tort  or otherwise, arising |
+| from,  out of  or in  connection with  the software or  the  use  or  other |
+| dealings in the software.                                                   |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| This  software is  available under the  three different licenses  mentioned |
+| below.  To use this software you must chose, and qualify, for one of those. |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| The WebFX Non-Commercial License          http://webfx.eae.net/license.html |
+| Permits  anyone the right to use the  software in a  non-commercial context |
+| free of charge.                                                             |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| The WebFX Commercial license           http://webfx.eae.net/commercial.html |
+| Permits the  license holder the right to use  the software in a  commercial |
+| context. Such license must be specifically obtained, however it's valid for |
+| any number of  implementations of the licensed software.                    |
+| - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - |
+| GPL - The GNU General Public License    http://www.gnu.org/licenses/gpl.txt |
+| Permits anyone the right to use and modify the software without limitations |
+| as long as proper  credits are given  and the original  and modified source |
+| code are included. Requires  that the final product, software derivate from |
+| the original  source or any  software  utilizing a GPL  component, such  as |
+| this, is also licensed under the GPL license.                               |
+|-----------------------------------------------------------------------------|
+| ????-??-?? | Original Version Posted.                                       |
+| 2003-09-25 | Added support for parseError getters. Also changed to extend   |
+|            | only XMLDocument instead of Document                           |
+|-----------------------------------------------------------------------------|
+| Created ????-??-?? | All changes are in the log above. | Updated 2003-09-25 |
+\----------------------------------------------------------------------------*/
+
+
 //////////////////
 // Helper Stuff //
 //////////////////
@@ -7,7 +53,7 @@
 function getDomDocumentPrefix() {
 	if (getDomDocumentPrefix.prefix)
 		return getDomDocumentPrefix.prefix;
-	
+
 	var prefixes = ["MSXML2", "Microsoft", "MSXML", "MSXML3"];
 	var o;
 	for (var i = 0; i < prefixes.length; i++) {
@@ -18,14 +64,14 @@ function getDomDocumentPrefix() {
 		}
 		catch (ex) {};
 	}
-	
+
 	throw new Error("Could not find an installed XML parser");
 }
 
 function getXmlHttpPrefix() {
 	if (getXmlHttpPrefix.prefix)
 		return getXmlHttpPrefix.prefix;
-	
+
 	var prefixes = ["MSXML2", "Microsoft", "MSXML", "MSXML3"];
 	var o;
 	for (var i = 0; i < prefixes.length; i++) {
@@ -36,7 +82,7 @@ function getXmlHttpPrefix() {
 		}
 		catch (ex) {};
 	}
-	
+
 	throw new Error("Could not find an installed XML parser");
 }
 
@@ -52,7 +98,7 @@ XmlHttp.create = function () {
 	try {
 		if (window.XMLHttpRequest) {
 			var req = new XMLHttpRequest();
-			
+
 			// some versions of Moz do not support the readyState property
 			// and the onreadystate event so we patch it!
 			if (req.readyState == null) {
@@ -63,7 +109,7 @@ XmlHttp.create = function () {
 						req.onreadystatechange();
 				}, false);
 			}
-			
+
 			return req;
 		}
 		if (window.ActiveXObject) {
@@ -83,7 +129,7 @@ XmlDocument.create = function () {
 		// DOM2
 		if (document.implementation && document.implementation.createDocument) {
 			var doc = document.implementation.createDocument("", "", null);
-			
+
 			// some versions of Moz do not support the readyState property
 			// and the onreadystate event so we patch it!
 			if (doc.readyState == null) {
@@ -94,7 +140,7 @@ XmlDocument.create = function () {
 						doc.onreadystatechange();
 				}, false);
 			}
-			
+
 			return doc;
 		}
 		if (window.ActiveXObject)
@@ -111,23 +157,23 @@ if (window.DOMParser &&
 
 	// XMLDocument did not extend the Document interface in some versions
 	// of Mozilla. Extend both!
-	XMLDocument.prototype.loadXML = 
-	Document.prototype.loadXML = function (s) {
-		
-		// parse the string to a new doc	
+	//XMLDocument.prototype.loadXML =
+	XMLDocument.prototype.loadXML = function (s) {
+
+		// parse the string to a new doc
 		var doc2 = (new DOMParser()).parseFromString(s, "text/xml");
-		
+
 		// remove all initial children
 		while (this.hasChildNodes())
 			this.removeChild(this.lastChild);
-			
+
 		// insert and import nodes
 		for (var i = 0; i < doc2.childNodes.length; i++) {
 			this.appendChild(this.importNode(doc2.childNodes[i], true));
 		}
 	};
-	
-	
+
+
 	/*
 	 * xml getter
 	 *
@@ -136,12 +182,51 @@ if (window.DOMParser &&
 	 * Usage: var sXml = oNode.xml
 	 *
 	 */
-	// XMLDocument did not extend the Document interface in some versions
-	// of Mozilla. Extend both!
-	XMLDocument.prototype.__defineGetter__("xml", function () {
+
+	// Node, should really be XMLNode but there is no such interface
+	Node.prototype.__defineGetter__("xml", function () {
 		return (new XMLSerializer()).serializeToString(this);
 	});
-	Document.prototype.__defineGetter__("xml", function () {
-		return (new XMLSerializer()).serializeToString(this);
+
+	XMLDocument.prototype.__defineGetter__("parseError", function () {
+		var hasError = !this.documentElement ||
+			this.documentElement.localName == "parsererror" &&
+			this.documentElement.getAttribute("xmlns") == "http://www.mozilla.org/newlayout/xml/parsererror.xml";
+		var res = {
+			errorCode:	0,
+			filepos:	0,	// not supported
+			line:		0,
+			linepos:	0,
+			reason:		"",
+			srcText:	"",
+			url:		""
+		};
+		if (hasError) {
+			res.errorCode = -1;
+			try {
+				res.srcText = this.getElementsByTagName("sourcetext")[0].firstChild.data;
+				res.srcText = res.srcText.replace( /\n\-\^$/, "");
+			}
+			catch (ex) {
+				res.srcText = "";
+			}
+
+			try {
+				// now we need to parse the first text node
+				var s = this.documentElement.firstChild.data;
+
+				var re = /XML Parsing Error\: (.+)\nLocation\: (.+)\nLine Number (\d+)\, Column (\d+)/;
+				var a = re.exec( s );
+				res.reason = a[1];
+				res.url = a[2];
+				res.line = a[3];
+				res.linepos = a[4];
+			}
+			catch (ex) {
+				res.reason = "Uknown";
+			}
+		}
+
+		return res;
 	});
 }
