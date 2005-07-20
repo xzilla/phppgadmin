a20852e34959d7bbdd9515d94284ecfa5a42f2b2
jollytoad <jollytoad>
2005-07-20 08:08:40 +0000

    Allow error text/icon to be customized.

--- xloadtree/xloadtree2.js
+++ xloadtree/xloadtree2.js
@@ -1,7 +1,7 @@
 /*----------------------------------------------------------------------------\
 |                        XLoadTree 2 PRE RELEASE                              |
 |                                                                             |
-| This is a pre release and redistribution is discouraged.                    |
+| This is a pre release and may not be redistributed.                         |
 | Watch http://webfx.eae.net for the final version                            |
 |                                                                             |
 |-----------------------------------------------------------------------------|
@@ -52,6 +52,9 @@
 
 webFXTreeConfig.loadingText = "Loading...";
 webFXTreeConfig.loadingIcon = "images/loading.gif";
+webFXTreeConfig.errorLoadingText = "Error Loading ";
+webFXTreeConfig.errorIcon = "images/exclamation.16.png";
+webFXTreeConfig.reloadText = "Click to reload";
 
 
 function WebFXLoadTree(sText, sXmlSrc, oAction, sBehavior, sIcon, sOpenIcon) {
@@ -388,9 +391,9 @@ WebFXLoadTree.documentLoaded = function (jsNode) {
 	// check that the load of the xml file went well
 	if(!doc || doc.parserError && doc.parseError.errorCode != 0 || !doc.documentElement) {
 		if (!doc || doc.parseError.errorCode == 0) {
-			jsNode.errorText = "Error loading " + jsNode.src + " (" + jsNode._xmlHttp.status + ": " + jsNode._xmlHttp.statusText + ")";
+			jsNode.errorText = webFXTreeConfig.errorLoadingText + jsNode.src + " (" + jsNode._xmlHttp.status + ": " + jsNode._xmlHttp.statusText + ")";
 		} else {
-			jsNode.errorText = "Error loading " + jsNode.src + " (" + doc.parseError.reason + ")";
+			jsNode.errorText = webFXTreeConfig.errorLoadingText + jsNode.src + " (" + doc.parseError.reason + ")";
 		}
 	} else {
 		// there is one extra level of tree elements
@@ -409,15 +412,15 @@ WebFXLoadTree.documentLoaded = function (jsNode) {
 
 		// if no children we got an error
 		if (count == 0) {
-			jsNode.errorText = "Error loading " + jsNode.src + " (???)";
+			jsNode.errorText = webFXTreeConfig.errorLoadingText + jsNode.src + " (???)";
 		}
 	}
 
 	if (jsNode.errorText != "") {
-		jsNode._loadingItem.icon = "images/exclamation.16.gif";
+		jsNode._loadingItem.icon = webFXTreeConfig.errorIcon;
 		jsNode._loadingItem.text = jsNode.errorText;
 		jsNode._loadingItem.action = WebFXLoadTree._reloadParent;
-		jsNode._loadingItem.toolTip = "Click to reload";
+		jsNode._loadingItem.toolTip = webFXTreeConfig.reloadText;
 
 		t.setSuspendRedraw(oldSuspend);
 
