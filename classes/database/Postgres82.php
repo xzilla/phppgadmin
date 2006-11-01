283d300e0d3c2e058c3611192c2bc1af3061ec1a
xzilla <xzilla>
2006-11-01 00:50:17 +0000

    updates for 8.2, per Javier Carlos

--- classes/database/Postgres82.php
+++ classes/database/Postgres82.php
@@ -3,7 +3,7 @@
 /**
  * PostgreSQL 8.2 support
  *
- * $Id: Postgres82.php,v 1.1 2005/11/08 02:24:31 chriskl Exp $
+ * $Id: Postgres82.php,v 1.2 2006/11/01 00:50:17 xzilla Exp $
  */
 
 include_once('./classes/database/Postgres81.php');
@@ -14,6 +14,36 @@ class Postgres82 extends Postgres81 {
 	
 	// Last oid assigned to a system object
 	var $_lastSystemOID = 17231;
+
+  	// List of all legal privileges that can be applied to different types
+  	// of objects.
+  	var $privlist = array(
+  		'table' => array('SELECT', 'INSERT', 'UPDATE', 'DELETE', 'RULE', 'REFERENCES', 'TRIGGER', 'ALL PRIVILEGES'),
+  		'view' => array('SELECT', 'INSERT', 'UPDATE', 'DELETE', 'RULE', 'REFERENCES', 'TRIGGER', 'ALL PRIVILEGES'),
+  		'sequence' => array('SELECT', 'UPDATE', 'ALL PRIVILEGES'),
+  		'database' => array('CREATE', 'TEMPORARY', 'CONNECT', 'ALL PRIVILEGES'),
+  		'function' => array('EXECUTE', 'ALL PRIVILEGES'),
+  		'language' => array('USAGE', 'ALL PRIVILEGES'),
+  		'schema' => array('CREATE', 'USAGE', 'ALL PRIVILEGES'),
+  		'tablespace' => array('CREATE', 'ALL PRIVILEGES')
+  	);
+  
+  	// List of characters in acl lists and the privileges they
+  	// refer to.
+  	var $privmap = array(
+  		'r' => 'SELECT',
+  		'w' => 'UPDATE',
+  		'a' => 'INSERT',
+  		'd' => 'DELETE',
+  		'R' => 'RULE',
+  		'x' => 'REFERENCES',
+  		't' => 'TRIGGER',
+  		'X' => 'EXECUTE',
+  		'U' => 'USAGE',
+  		'C' => 'CREATE',
+  		'T' => 'TEMPORARY',
+  		'c' => 'CONNECT'
+  	);	
 
 	/**
 	 * Constructor
