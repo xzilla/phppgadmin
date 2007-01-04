42c7840cd0235ff90d81f662ec65fc92c9859eb0
soranzo <soranzo>
2007-01-04 19:12:31 +0000

    Adapt to our code style

--- autocomplete.php
+++ autocomplete.php
@@ -1,11 +1,11 @@
 <?php
-include_once("libraries/lib.inc.php");
-$szSQL = "select * from ". $data->clean($_REQUEST["tb"]) ." where ". $data->clean($_REQUEST["fk"]) ." LIKE '". $data->clean($_REQUEST["v"]) ."%' LIMIT 11";
-$objRes = $data->selectSet($szSQL);
-$arrayRes = array();
-while(!$objRes->EOF) {
-	$arrayRes[] = $objRes->fields[$_REQUEST["fk"]];
-	$objRes->moveNext();
-}
-echo implode("PPA_EOF;|",$arrayRes);
+	include_once('libraries/lib.inc.php');
+	$szSQL = 'SELECT * FROM ' . $data->clean($_REQUEST['tb']) . ' WHERE ' . $data->clean($_REQUEST['fk']) . " LIKE '" . $data->clean($_REQUEST['v']) . "%' LIMIT 11";
+	$objRes = $data->selectSet($szSQL);
+	$arrayRes = array();
+	while (!$objRes->EOF) {
+		$arrayRes[] = $objRes->fields[$_REQUEST['fk']];
+		$objRes->moveNext();
+	}
+	echo implode('PPA_EOF;|', $arrayRes);
 ?>
