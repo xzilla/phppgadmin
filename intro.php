1c58410b8c4ff486c132c21766e908de781a528d
soranzo <soranzo>
2007-04-20 18:30:51 +0000

    XHTML Strict fixes.

--- intro.php
+++ intro.php
@@ -3,7 +3,7 @@
 	/**
 	 * Intro screen
 	 *
-	 * $Id: intro.php,v 1.16 2006/12/31 16:58:59 soranzo Exp $
+	 * $Id: intro.php,v 1.17 2007/04/20 18:30:51 soranzo Exp $
 	 */
 
 	// Include application functions (no db conn)
@@ -20,26 +20,26 @@
 <h1><?php echo "$appName $appVersion (PHP ". phpversion() .')' ?></h1>
 
 <form method="get" action="<?php echo $_SERVER['PHP_SELF'] ?>">
-  <select name="language" onchange="this.form.submit()">
+	<p><select name="language" onchange="this.form.submit()">
 <?php
 	$language = isset($_SESSION['webdbLanguage']) ? $_SESSION['webdbLanguage'] : 'english';
 	foreach ($appLangFiles as $k => $v) {
-		echo "<option value=\"{$k}\"",
+		echo "\t<option value=\"{$k}\"",
 			($k == $language) ? ' selected="selected"' : '',
 			">{$v}</option>\n";
 	}
 ?>
-  </select>
-  <noscript><input type="submit" value="<?php echo $lang['stralter'] ?>" /></noscript>
+	</select></p>
+	<noscript><p><input type="submit" value="<?php echo $lang['stralter'] ?>" /></p></noscript>
 </form>
 
 <p><?php echo $lang['strintro'] ?></p>
 
 <ul class="intro">
-<li><a href="http://phppgadmin.sourceforge.net/" target="_top"><?php echo $lang['strppahome'] ?></a></li>
-<li><a href="<?php echo $lang['strpgsqlhome_url'] ?>" target="_top"><?php echo $lang['strpgsqlhome'] ?></a></li>
-<li><a href="http://sourceforge.net/tracker/?group_id=37132&amp;atid=418980" target="_top"><?php echo $lang['strreportbug'] ?></a></li>
-<li><a href="<?php echo $lang['strviewfaq_url'] ?>" target="_top"><?php echo $lang['strviewfaq'] ?></a></li>
+	<li><a href="http://phppgadmin.sourceforge.net/"><?php echo $lang['strppahome'] ?></a></li>
+	<li><a href="<?php echo $lang['strpgsqlhome_url'] ?>"><?php echo $lang['strpgsqlhome'] ?></a></li>
+	<li><a href="http://sourceforge.net/tracker/?group_id=37132&amp;atid=418980"><?php echo $lang['strreportbug'] ?></a></li>
+	<li><a href="<?php echo $lang['strviewfaq_url'] ?>"><?php echo $lang['strviewfaq'] ?></a></li>
 </ul>
 
 <?php
