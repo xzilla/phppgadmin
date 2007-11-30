d95fd2005e0d1e43fb45f2f012cfcf5049862b6f
soranzo <soranzo>
2007-11-30 15:27:26 +0000

    Update help_base parameters

--- help/PostgresDoc82.php
+++ help/PostgresDoc82.php
@@ -3,11 +3,11 @@
 /**
  * Help links for PostgreSQL 8.2 documentation
  *
- * $Id: PostgresDoc82.php,v 1.2 2006/04/21 03:31:26 chriskl Exp $
+ * $Id: PostgresDoc82.php,v 1.3 2007/11/30 15:27:26 soranzo Exp $
  */
 
 include('./help/PostgresDoc81.php');
 
-$this->help_base = sprintf($GLOBALS['conf']['help_base'], 'current');
+$this->help_base = sprintf($GLOBALS['conf']['help_base'], '8.2');
 
 ?>
