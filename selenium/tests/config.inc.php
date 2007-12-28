7356a99a7cb28ecdb929435da2ec9839f669a34b
ioguix <ioguix>
2007-12-28 16:56:09 +0000

    some selenium tests cleanup

--- selenium/tests/config.inc.php
+++ selenium/tests/config.inc.php
@@ -1,8 +1,8 @@
 <?php
 require('../../lang/recoded/english.php');
 
-$webUrl = 'http://127.0.0.1/~ioguixigx/ppa/ppa.test';
-$serverName = 'pg83'; // one of your $conf['servers'][*]['desc'] in conf/config.inc/php
+$webUrl = 'http://boox/~ioguix/ppa/ppa.test';
+$serverName = '8.3'; // one of your $conf['servers'][*]['desc'] in conf/config.inc/php
 #deprecated $superuser = 'ppatests_super'; // according to your selenium/tests/data/config.sql
 #deprecated $superpass = 'super'; // according to your selenium/tests/data/config.sql
 $superuser = 'postgres'; //only use to create and drop the following admin role
--- selenium/tests/index.php
+++ selenium/tests/index.php
@@ -7,7 +7,7 @@
 <body>
 <table cellpadding="1" cellspacing="1" border="1">
 <thead>
-<tr><td rowspan="1" colspan="3">New Test</td></tr>
+<tr><td rowspan="1" colspan="3">Create/Drop an unique index</td></tr>
 </thead><tbody>
 <?php include('login.php') ?>
 <tr>
@@ -42,17 +42,47 @@
 </tr>
 <tr>
 	<td>clickAndWait</td>
-	<td>link=<?php echo $lang['strcolumns'] ?></td>
+	<td>link=<?php echo $lang['strindexes'] ?></td>
 	<td></td>
 </tr>
 <tr>
 	<td>clickAndWait</td>
-	<td>link=altered_col</td>
+	<td>link=<?php echo $lang['strcreateindex'] ?></td>
+	<td></td>
+</tr>
+<tr>
+	<td>type</td>
+	<td>formIndexName</td>
+	<td>name_unique</td>
+</tr>
+<tr>
+	<td>addSelection</td>
+	<td>TableColumnList</td>
+	<td>label=name</td>
+</tr>
+<tr>
+	<td>click</td>
+	<td>add</td>
 	<td></td>
 </tr>
 <tr>
 	<td>clickAndWait</td>
-	<td>link=<?php echo $lang['strdrop'] ?></td>
+	<td>//input[@value='<?php echo $lang['strcreate'] ?>']</td>
+	<td></td>
+</tr>
+<tr>
+	<td>assertText</td>
+	<td>//p[@class='message']</td>
+	<td><?php echo $lang['strindexcreated'] ?></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>link=<?php echo $lang['strindexes'] ?></td>
+	<td></td>
+</tr>
+<tr>
+	<td>clickAndWait</td>
+	<td>//tr/td[text()='name_unique']/../td/a[text()='Drop']</td>
 	<td></td>
 </tr>
 <tr>
@@ -68,7 +98,7 @@
 <tr>
 	<td>assertText</td>
 	<td>//p[@class='message']</td>
-	<td><?php echo $lang['strcolumndropped'] ?></td>
+	<td><?php echo $lang['strindexdropped'] ?></td>
 </tr>
 <?php include('logout.php'); ?>
 </tbody></table>
