9b6e7f215697eec1004831706e9386809613d1bb
ioguix <ioguix>
2008-04-14 13:52:40 +0000

    fix table icon/link disapearing when navigate between pages during column browse

--- display.php
+++ display.php
@@ -9,7 +9,7 @@
 	 * @param $return_desc The return link name
 	 * @param $page The current page
 	 *
-	 * $Id: display.php,v 1.68 2008/04/14 12:44:27 ioguix Exp $
+	 * $Id: display.php,v 1.67.2.1 2008/04/14 13:52:40 ioguix Exp $
 	 */
 
 	// Prevent timeouts on large exports (non-safe mode only)
