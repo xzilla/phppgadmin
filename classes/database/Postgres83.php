331e91c655bbadbe633f0c723500899726d02810
Guillaume (ioguix) de Rorthais <ioguix@free.fr>
2010-12-21 00:23:46 +0100

    Fix bad inheritance between pg83 -> pg instead of pg83 -> pg84 in the database access classes

--- classes/database/Postgres83.php
+++ classes/database/Postgres83.php
@@ -6,9 +6,9 @@
  * $Id: Postgres82.php,v 1.10 2007/12/28 16:21:25 ioguix Exp $
  */
 
-include_once('./classes/database/Postgres.php');
+include_once('./classes/database/Postgres84.php');
 
-class Postgres83 extends Postgres {
+class Postgres83 extends Postgres84 {
 
 	var $major_version = 8.3;
 
