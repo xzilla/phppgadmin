61b1c52ad6699679df2129579fad7a583b2a387f
Jehan-Guillaume (ioguix) de Rorthais <jgdr@dalibo.com>
2011-06-01 21:32:04 +0200

    Show cancel/kill actions in process page only if role is a superuser.

--- database.php
+++ database.php
@@ -457,23 +457,26 @@
 
 		// Build possible actions for our process list
 		$columns['actions'] = array('title' => $lang['stractions']);
-			
-		$actions = array(
-			'cancel' => array(
-				'title' => $lang['strcancel'],
-				'url'   => "database.php?action=signal&amp;signal=CANCEL&amp;{$misc->href}&amp;",
-				'vars'  => array('procpid' => 'procpid')
-			),
-			'kill' => array(
-				'title' => $lang['strkill'],
-				'url'   => "database.php?action=signal&amp;signal=KILL&amp;{$misc->href}&amp;",
-				'vars'  => array('procpid' => 'procpid')
-			)
-		);
+
+		$actions = array();
+		if ($data->isSuperUser()) {
+			$actions = array(
+				'cancel' => array(
+					'title' => $lang['strcancel'],
+					'url'   => "database.php?action=signal&amp;signal=CANCEL&amp;{$misc->href}&amp;",
+					'vars'  => array('procpid' => 'procpid')
+				),
+				'kill' => array(
+					'title' => $lang['strkill'],
+					'url'   => "database.php?action=signal&amp;signal=KILL&amp;{$misc->href}&amp;",
+					'vars'  => array('procpid' => 'procpid')
+				)
+			);
 	
-		// Remove actions where not supported
-		if (!$data->hasQueryKill()) unset($actions['kill']);
-		if (!$data->hasQueryCancel()) unset($actions['cancel']);
+			// Remove actions where not supported
+			if (!$data->hasQueryKill()) unset($actions['kill']);
+			if (!$data->hasQueryCancel()) unset($actions['cancel']);
+		}
 
 		if (count($actions) == 0) unset($columns['actions']);
 	
