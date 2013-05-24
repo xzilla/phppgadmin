xzilla/phppgadmin

commit d571ecae7b73bb3bd1acdc2b84c230c9b623154c

Fix bug with getProcesses with 9.2

classes/database/Postgres91.php
@@ -35,15 +35,20 @@ class Postgres91 extends Postgres {
 	 */
 	function getProcesses($database = null) {
 		if ($database === null)
-			$sql = "SELECT * FROM pg_catalog.pg_stat_activity ORDER BY datname, usename, procpid";
+			$sql = "SELECT datname, usename, procpid AS pid, current_query AS query, query_start
+				FROM pg_catalog.pg_stat_activity
+				ORDER BY datname, usename, procpid";
 		else {
-			$this->clean($database);
-		$sql = "
-				SELECT * FROM pg_catalog.pg_stat_activity
-				WHERE datname='{$database}' ORDER BY usename, procpid";
+			//$this->clean($database);
+			$sql = "SELECT datname, usename, procpid AS pid, current_query AS query, query_start
+				FROM pg_catalog.pg_stat_activity
+				-- WHERE datname='{$database}'
+				ORDER BY usename, procpid";
 		}
 
-		return $this->selectSet($sql);
+		$rc = $this->selectSet($sql);
+
+		return $rc;
 	}
 
 	// Tablespace functions

classes/database/Postgres.php
@@ -7196,12 +7196,15 @@ class Postgres extends ADODB_base {
 	 */
 	function getProcesses($database = null) {
 		if ($database === null)
-			$sql = "SELECT * FROM pg_catalog.pg_stat_activity ORDER BY datname, usename, pid";
+			$sql = "SELECT datname, usename, pid, query, query_start
+				FROM pg_catalog.pg_stat_activity
+				ORDER BY datname, usename, pid";
 		else {
 			$this->clean($database);
-		$sql = "
-				SELECT * FROM pg_catalog.pg_stat_activity
-				WHERE datname='{$database}' ORDER BY usename, pid";
+			$sql = "SELECT datname, usename, pid, query, query_start
+				FROM pg_catalog.pg_stat_activity
+				WHERE datname='{$database}'
+				ORDER BY usename, pid";
 		}
 
 		return $this->selectSet($sql);

database.php
@@ -435,7 +435,7 @@
 		// Fetch the processes from the database
 		echo "<h3>{$lang['strprocesses']}</h3>\n";
 		$processes = $data->getProcesses($_REQUEST['database']);
-				
+
 		$columns = array(
 			'user' => array(
 				'title' => $lang['strusername'],
@@ -443,11 +443,11 @@
 			),
 			'process' => array(
 				'title' => $lang['strprocess'],
-				'field' => field('procpid'),
+				'field' => field('pid'),
 			),
 			'query' => array(
 				'title' => $lang['strsql'],
-				'field' => field('current_query'),
+				'field' => field('query'),
 			),
 			'start_time' => array(
 				'title' => $lang['strstarttime'],
@@ -496,9 +496,6 @@
 
 		if (count($actions) == 0) unset($columns['actions']);
 	
-		// Remove query start time for <7.4
-		if (!isset($processes->fields['query_start'])) unset($columns['start_time']);
-
 		$misc->printTable($processes, $columns, $actions, 'database-processes', $lang['strnodata']);
 		
 		if ($isAjax) exit;

commit 7ef555fb076bfbaedf8028905b2b03ec4ae1eb98

Processes: Fix <= 9.1 to show only db related processes & clean dbname.
Fix bugs introduced in commit d571ecae7b by ioguix on 2012-09-21.

I can't tell why these changes crept in along with the rest of
the fixes in that commit.  It looks like a testing version was
committed.

classes/database/Postgres91.php
@@ -39,10 +39,10 @@ class Postgres91 extends Postgres92 {
 				FROM pg_catalog.pg_stat_activity
 				ORDER BY datname, usename, procpid";
 		else {
-			//$this->clean($database);
+			$this->clean($database);
 			$sql = "SELECT datname, usename, procpid AS pid, current_query AS query, query_start
 				FROM pg_catalog.pg_stat_activity
-				-- WHERE datname='{$database}'
+				WHERE datname='{$database}'
 				ORDER BY usename, procpid";
 		}
 