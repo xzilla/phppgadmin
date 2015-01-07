306850c9bb642766476482b1098a1eff96ba97d4
dMagsCan <can@dmags.net>
2015-01-07 14:03:08 +0200

    Update turkish.php
    The empty line end of the file causing to fail ajax response that containts xml header

--- lang/turkish.php
+++ lang/turkish.php
@@ -639,4 +639,3 @@ $lang['strcannotdumponwindows']  =  'Dumping of complex table and schema names o
 	$lang['strerrorloading']  =  'Yükleme hatası';
 	$lang['strclicktoreload']  =  'Yeniden yüklemek için tıklayın.';
 ?>
-
