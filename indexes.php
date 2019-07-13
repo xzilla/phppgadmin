ec53557fc821b820462d663ece30cb7828bb4757
Robert Treat <rob@xzilla.net>
2019-07-13 12:24:30 -0400

    Fix pass by reference warnings in index creation.

--- indexes.php
+++ indexes.php
@@ -91,7 +91,8 @@
 
 		if ($attrs->recordCount() > 0) {
 			while (!$attrs->EOF) {
-				$selColumns->add(new XHTML_Option($attrs->fields['attname']));
+                $XHTML_Option = new XHTML_Option($attrs->fields['attname']);
+                $selColumns->add($XHTML_Option);
 				$attrs->moveNext();
 			}
 		}
